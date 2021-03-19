c9e2c3a6238e0957b748b963ccfa8133fb049496
fredyunivalle <fredy.ballesteros@correounivalle.edu.co>
2021-03-18 21:06:56 -0500

    Primer envio

---
+++ routers/basedatos.js
@@ -0,0 +1,31 @@
+const { Client } = require('pg');
+const Router = require('express-promise-router');
+var config = require('../config');
+
+
+const client = new Client({
+  connectionString: config.postgrest.url,
+  ssl: {
+    rejectUnauthorized: false
+  }
+});
+
+client.connect();
+
+const router = new Router();
+// export our router to be mounted by the parent application
+module.exports = router;
+
+router.get('/consultatotalpacientes', async (req, res) => {
+  //const { id } = req.params
+  const { rows } = await client.query('SELECT * FROM pacientes');
+  res.send(rows);
+});
+
+router.post('/insertarpacientes', async (req, res) => {
+  const { nombre, apellido, numid } = req.body;
+  await client.query(
+    `INSERT INTO pacientes(numid, nombre, apellidos) VALUES('${nombre}','${apellido}','${numid}')`
+  );
+  res.send('INSERTADO');
+});
